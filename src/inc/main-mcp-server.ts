0255c184083921ae062aaa3580fab2dcdc772905
lamemind <lamemind@gmail.com>
2025-05-27 17:55:19 +0200

    fix: placeholder resource and prompt to workaround claude destop error logs

--- src/inc/main-mcp-server.ts
+++ src/inc/main-mcp-server.ts
@@ -1,8 +1,7 @@
-import {z} from "zod";
-import {getToolName, registerWrappedServer} from "./wrapped-servers.js";
-import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
-import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
-import {WrapperConfig} from "./json-config.js";
+import { registerWrappedServer } from "./wrapped-servers.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { WrapperConfig } from "./json-config.js";
 
 async function startMainServer(config: WrapperConfig) {
     console.error(`Starting main server with name: ${config.serverName}`);
@@ -10,7 +9,36 @@ async function startMainServer(config: WrapperConfig) {
     const mainMcpServer = new McpServer({
         name: config.serverName,
         version: "1.0.0"
+    }, {
+        capabilities: {
+            prompts: {},
+            resources: {}
+        }
     });
+
+    mainMcpServer.prompt(
+        `${config.serverName}_placeholder`, {},
+        ({ }) => ({
+            messages: [{
+                role: "user",
+                content: {
+                    type: "text",
+                    text: `this is a placeholder prompt to avoid error logs when no prompts are registered`
+                }
+            }]
+        })
+    );
+
+    mainMcpServer.resource(
+        `${config.serverName}_placeholder`,
+        `placeholder://${config.serverName}`,
+        async (uri) => ({
+            contents: [{
+                uri: uri.href,
+                text: `This is a placeholder resource to avoid error logs when no resources are registered`
+            }]
+        })
+    );
 
     for (const wrappedConfig of config.servers) {
         await registerWrappedServer(mainMcpServer, config, wrappedConfig);
@@ -22,4 +50,4 @@ async function startMainServer(config: WrapperConfig) {
     console.error("Server started successfully");
 }
 
-export {startMainServer};
+export { startMainServer };
